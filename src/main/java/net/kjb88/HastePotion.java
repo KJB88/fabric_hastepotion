bc36392b9964f7ecc351a9b0e9cb5eea2e86df11
Kevin <kevin.barr88@gmail.com>
2024-04-16 17:34:34 +0100

    Haste Potion registered and tested

--- src/main/java/net/kjb88/HastePotion.java
+++ src/main/java/net/kjb88/HastePotion.java
@@ -2,17 +2,20 @@ package net.kjb88;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.kjb88.potion.hastePotion;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class HastePotion implements ModInitializer {
+	public static final String MOD_ID = "haste_potion";
 	// This logger is used to write text to the console and the log file.
 	// It is considered best practice to use your mod id as the logger's name.
 	// That way, it's clear which mod wrote info, warnings, and errors.
-    public static final Logger LOGGER = LoggerFactory.getLogger("haste_potion");
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID );
 
 	@Override
 	public void onInitialize() {
+		hastePotion.registerPotions();
 		// This code runs as soon as Minecraft is in a mod-load-ready state.
 		// However, some things (like resources) may still be uninitialized.
 		// Proceed with mild caution.
---
+++ src/main/java/net/kjb88/potion/hastePotion.java
@@ -0,0 +1,60 @@
+package net.kjb88.potion;
+
+
+import net.kjb88.mixin.BrewingRecipeRegistryMixin;
+import net.minecraft.entity.effect.StatusEffectInstance;
+import net.minecraft.entity.effect.StatusEffects;
+import net.minecraft.item.Items;
+import net.minecraft.potion.Potion;
+import net.minecraft.potion.Potions;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
+
+import static net.kjb88.HastePotion.MOD_ID;
+
+public class hastePotion {
+    public static Potion HASTE_POTION;
+    public static Potion LONG_HASTE_POTION;
+    public static Potion STRONG_HASTE_POTION;
+
+    // Register the standard Haste Potion
+    public static Potion registerHastePotion(String name){
+        return Registry.register(
+                Registries.POTION,
+                new Identifier(MOD_ID, name),
+                new Potion(new StatusEffectInstance(
+                        StatusEffects.HASTE, 3600, 1)));
+    }
+    // Register the Long Haste Potion
+    public static Potion registerLongHastePotion(String name){
+        return Registry.register(
+                Registries.POTION,
+                new Identifier(MOD_ID, name),
+                new Potion(new StatusEffectInstance(
+                        StatusEffects.HASTE, 9600, 1)));
+    }
+    // Register the Strong Haste Potion
+    public static Potion registerStrongHastePotion(String name){
+        return Registry.register(
+                Registries.POTION,
+                new Identifier(MOD_ID, name),
+                new Potion(new StatusEffectInstance(
+                        StatusEffects.HASTE, 3600, 4)));
+    }
+
+    public static void registerPotions()
+    {
+        HASTE_POTION = registerHastePotion("haste_potion");
+        LONG_HASTE_POTION = registerLongHastePotion("long_haste_potion");
+        STRONG_HASTE_POTION = registerStrongHastePotion("strong_haste_potion");
+        registerPotionRecipes();
+    }
+
+    private static void registerPotionRecipes()
+    {
+        BrewingRecipeRegistryMixin.invokeRegisterPotionRecipe(Potions.AWKWARD, Items.GOLDEN_APPLE, hastePotion.HASTE_POTION);
+        BrewingRecipeRegistryMixin.invokeRegisterPotionRecipe(hastePotion.HASTE_POTION, Items.REDSTONE, hastePotion.LONG_HASTE_POTION);
+        BrewingRecipeRegistryMixin.invokeRegisterPotionRecipe(hastePotion.HASTE_POTION, Items.GLOWSTONE_DUST, hastePotion.STRONG_HASTE_POTION);
+    }
+}
